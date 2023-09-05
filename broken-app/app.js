9c99da5f1eb1f742d23a68da259edaca38964a16
Tyson <trantyson@icloud.com>
2023-09-05 14:14:00 -0700

    finish debugging the app

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,18 +1,28 @@
-const express = require('express');
-let axios = require('axios');
+const express = require("express");
+let axios = require("axios");
 var app = express();
 
-app.post('/', function(req, res, next) {
+//Second Bug
+app.use(express.json());
+
+//Third Bug
+app.post("/", async function (req, res, next) {
   try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
+    let results = req.body.developers.map((d) => {
+      return axios.get(`https://api.github.com/users/${d}`);
     });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+    let fulfilled = await Promise.all(results);
+
+    let out = fulfilled.map((r) => ({ name: r.data.name, bio: r.data.bio }));
 
     return res.send(JSON.stringify(out));
-  } catch {
+  } catch (err) {
+    //First Bug
     next(err);
   }
 });
 
+app.use((err, req, res, next) => {
+  res.status(500).send({ error: err.message });
+});
 app.listen(3000);
--- broken-app/issues.md
+++ broken-app/issues.md
@@ -1,1 +1,8 @@
 # Broken App Issues
+1. ReferenceError: err is not defined
+   - The **catch** code is not catching any errors
+2. TypeError: Cannot read property 'developers' of undefined
+   - The req.body was undefined, due to the app not being set up properly for Express to parse incoming JSON
+3. TypeError
+   - Removed the async and await from the first map function in the variable result
+   - We also turn results which was a array of promises, and use Promise.all() to fulfill those promises before we use the variable out to map them
